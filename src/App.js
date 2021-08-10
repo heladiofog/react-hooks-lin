a44eb50dd79e65374ea0a1ca7aa2737d5af7a653
Heladio Atreaga Franco <harteagaf@atencion.com>
2021-08-09 21:50:00 -0500

    Refactoring for using useReducer hook instead of useState.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  // Refactoring this functionality with useReducer instead:
+  // const [checked, setChecked] = useState(false);
+  // into:
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  )
 
   return (
     <div className="App">
       <h1>The package is:</h1>
-      <input 
+      <input
         type="checkbox"
         value={checked}
         // A little enhancement for the value setting
-        onChange={() => setChecked(checked => !checked)}
+        // onChange={() => setChecked(checked => !checked)}
+        // New chang logic handled by useReducer hook
+        onChange={toggle}
       />
       <p>{checked ? "Checked" : "not checked"}</p>
     </div>
