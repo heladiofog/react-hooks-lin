049f035e6f224ffe0cb27824ce50f98f3da67b12
Heladio Atreaga Franco <harteagaf@atencion.com>
2021-08-09 20:45:23 -0500

    Showing related effects fired by state values.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState('Lin');
+  const [isAdmin, setIsAdmin] = useState(false);
 
   // UseEffect example, it runs after each render!! in every object
   useEffect(() => {
-    document.title = `Celebrate ${name}`;
-  });
+    // document.title = `Celebrate ${name}`;
+    console.log(`Celebrate ${name}`)
+  }, [name]); // If the array is empty, the effect only fires once, on first render
+
+  // We just want the isAdmin value change to fire the related effect
+  useEffect(() => {
+    console.log(`The user is: ${isAdmin ? 'Admin' : 'No admin'}`);
+  } ,[isAdmin]);
 
   return (
     <div className="App">
@@ -15,6 +22,9 @@ function App() {
         <h1>Congratulations {name}!</h1>
         <button onClick={() => setName("Tezca")}>Change Winner name</button>
       </section>
+
+      <p>{isAdmin ? "Logged In": "Not Logged In" }</p>
+      <button onClick={() => setIsAdmin(true)}>Log In</button>
     </div>
   );
 }
