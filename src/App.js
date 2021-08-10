2f94ad63d71a1b6c86ac7d49d2bb1bf6ae03081a
Heladio Atreaga Franco <harteagaf@atencion.com>
2021-08-09 20:29:29 -0500

    Enters UseEffect hook!

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { FaStar } from 'react-icons/fa';
-
-// Helper function to create an array of stars
-const createArray = (length) => [
-  ...Array(length)
-]
-
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? 'gold' : 'grey'} onClick={onSelect} />;
-}
-
-function StarRating({ totalStars = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return(
-  <>
-    {createArray(totalStars).map((star, i) => (
-      <Star
-        key={`${i}`}
-        selected={selectedStars > i}
-        onSelect={() => setSelectedStars(i + 1)}
-      />
-    ))}
-    <p>{selectedStars} of {totalStars}</p>
-  </>);
-}
 
 function App() {
+  const [name, setName] = useState('Lin');
+
+  // UseEffect example, it runs after each render!! in every object
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  });
+
   return (
     <div className="App">
-      <h1>Rating App</h1>
-      <h3>Star Trek rating:</h3>
-      <StarRating totalStars={10} />
+      <section>
+        <h1>Congratulations {name}!</h1>
+        <button onClick={() => setName("Tezca")}>Change Winner name</button>
+      </section>
     </div>
   );
 }
