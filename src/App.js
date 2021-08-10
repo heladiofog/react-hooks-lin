ad5af362dab78f8bd0608e53951ac36b9c08e03b
Heladio Atreaga Franco <harteagaf@atencion.com>
2021-08-10 10:34:34 -0500

    Enters the React Context Provider and Hook fot sharing values to the entire app components tree.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
-import { useInput } from './hooks/useInput';
-// Extract/abstract logic into a Custom hook.
-// Refactoring the Controlled component example into a "hook-controlled component"
+import { TreesContext } from './';  // index file
+
 function App() {
-  // Instead of using inputs, we're going to use the input hook
-  const [soundProps, resetSound] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`${soundProps.value} sounds like ${colorProps.value}.`);
-    // Reset the input fields
-    resetSound("");
-    resetColor("#000000");
-  }
-
+  // const treesContext = useContext(TreesContext);
+  const { trees } = useContext(TreesContext);
+  // console.log(trees);
   return (
     <div className="App">
-      <h1>Use of a Controlled component and a Custom Hook</h1>
-      <p>Fill the inputs in the following form:</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Sound..."
-          // These two props will be replaced for the provided ones by the hook
-          // value={sound}
-          // onChange={(e) => setSound(e.target.value)}
-          {...soundProps}
-        />
-        <input
-          type="color"
-          // These two props will be replaced for the provided ones by the hook
-          // value={color}
-          // onChange={(e) => setColor(e.target.value)}
-          {...colorProps}
-        />
-        <button type="submit">Send</button>
-      </form>
+      <h1>Use of Context to share some values</h1>
+      <h3>Trees I've heard of:</h3>
+      <ul>
+        {trees.map(tree => (<li key={tree.id}>{tree.type}</li>))}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Share values to be available through the components tree
+// These values are passed down with react context
+export const TreesContext = createContext();
+
+// Trees example to be accessible to our entire app
+const trees = [
+  { id: "1", type: "Maple" },
+  { id: "2", type: "Oak" },
+  { id: "3", type: "Family" },
+  { id: "4", type: "Components" },
+  { id: "5", type: "Mango" },
+];
+
 ReactDOM.render(
   <React.StrictMode>
+    <TreesContext.Provider value={{ trees }} >
     <App name="Lin"/>
+    </TreesContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
