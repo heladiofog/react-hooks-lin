1d05c603fc978c7082ea7c0481bb2d2c984a1fab
Heladio Atreaga Franco <harteagaf@atencion.com>
2021-08-09 20:22:29 -0500

    Rating app for showing another example of useState.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { FaStar } from 'react-icons/fa';
+
+// Helper function to create an array of stars
+const createArray = (length) => [
+  ...Array(length)
+]
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? 'gold' : 'grey'} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return(
+  <>
+    {createArray(totalStars).map((star, i) => (
+      <Star
+        key={`${i}`}
+        selected={selectedStars > i}
+        onSelect={() => setSelectedStars(i + 1)}
+      />
+    ))}
+    <p>{selectedStars} of {totalStars}</p>
+  </>);
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
-
   return (
     <div className="App">
-      <h1>The package is:</h1>
-      <input 
-        type="checkbox"
-        value={checked}
-        // A little enhancement for the value setting
-        onChange={() => setChecked(checked => !checked)}
-      />
-      <p>{checked ? "Checked" : "not checked"}</p>
+      <h1>Rating App</h1>
+      <h3>Star Trek rating:</h3>
+      <StarRating totalStars={10} />
     </div>
   );
 }
